6aab74572dde9e59d5c9cf685256f454e2d3dcf8
Chomtana <Chomtana001@gmail.com>
2022-03-28 01:01:34 +0700

    Connect to Keplr

--- src/pages/WalletConnect.jsx
+++ src/pages/WalletConnect.jsx
@@ -23,6 +23,7 @@ const WalletConnect = () => {
   const [web3Account, setWeb3Account] = useState(null);
   const [userBalance, setUserBalance] = useState(null);
   const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+  const [keplrAddress, setKeplrAddress] = useState("");
 
   const {
     status,
@@ -94,6 +95,127 @@ const WalletConnect = () => {
     connect("EXTENSION");
   }
 
+  async function refreshKeplr() {
+    const chainId = "bombay-12";
+    const offlineSigner = window.keplr.getOfflineSigner(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    setKeplrAddress(accounts[0]?.address || "");
+  }
+
+  async function connectKeplr() {
+    if (!window.keplr) {
+      window.alert("Please install Keplr wallet");
+      return;
+    }
+
+    const chainId = "bombay-12";
+
+    window.keplr.experimentalSuggestChain({
+      // Chain-id of the Cosmos SDK chain.
+      chainId,
+      // The name of the chain to be displayed to the user.
+      chainName: "Terra Bombay-12",
+      // RPC endpoint of the chain.
+      rpc: "https://bombay.stakesystems.io:2053",
+      // REST endpoint of the chain.
+      rest: "https://bombay-lcd.terra.dev",
+      // Staking coin information
+      stakeCurrency: {
+          // Coin denomination to be displayed to the user.
+          coinDenom: "LUNA",
+          // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+          coinMinimalDenom: "uluna",
+          // # of decimal points to convert minimal denomination to user-facing denomination.
+          coinDecimals: 6,
+          // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+          // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+          coinGeckoId: "terra-luna"
+      },
+      // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
+      // The 'stake' button in Keplr extension will link to the webpage.
+      // walletUrlForStaking: "",
+      // The BIP44 path.
+      bip44: {
+          // You can only set the coin type of BIP44.
+          // 'Purpose' is fixed to 44.
+          coinType: 118,
+      },
+      // Bech32 configuration to show the address to user.
+      // This field is the interface of
+      // {
+      //   bech32PrefixAccAddr: string;
+      //   bech32PrefixAccPub: string;
+      //   bech32PrefixValAddr: string;
+      //   bech32PrefixValPub: string;
+      //   bech32PrefixConsAddr: string;
+      //   bech32PrefixConsPub: string;
+      // }
+      bech32Config: {
+          bech32PrefixAccAddr: "terra",
+          bech32PrefixAccPub: "terrapub",
+          bech32PrefixValAddr: "terravaloper",
+          bech32PrefixValPub: "terravaloperpub",
+          bech32PrefixConsAddr: "terravalcons",
+          bech32PrefixConsPub: "terravalconspub"
+      },
+      // List of all coin/tokens used in this chain.
+      currencies: [{
+          // Coin denomination to be displayed to the user.
+          coinDenom: "LUNA",
+          // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+          coinMinimalDenom: "uluna",
+          // # of decimal points to convert minimal denomination to user-facing denomination.
+          coinDecimals: 6,
+          // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+          // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+          coinGeckoId: "terra-luna",
+      },
+      {
+          // Coin denomination to be displayed to the user.
+          coinDenom: "UST",
+          // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+          coinMinimalDenom: "uusd",
+          // # of decimal points to convert minimal denomination to user-facing denomination.
+          coinDecimals: 6,
+          // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+          // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+          // coinGeckoId: "terra-luna",
+      }
+      ],
+      // List of coin/tokens used as a fee token in this chain.
+      feeCurrencies: [{
+          // Coin denomination to be displayed to the user.
+          coinDenom: "LUNA",
+          // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+          coinMinimalDenom: "uluna",
+          // # of decimal points to convert minimal denomination to user-facing denomination.
+          coinDecimals: 6,
+          // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+          // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+          coinGeckoId: "terra-luna"
+      }],
+      // (Optional) The number of the coin type.
+      // This field is only used to fetch the address from ENS.
+      // Ideally, it is recommended to be the same with BIP44 path's coin type.
+      // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
+      // So, this is separated to support such chains.
+      coinType: 118,
+      // (Optional) This is used to set the fee of the transaction.
+      // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
+      // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
+      // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
+      gasPriceStep: {
+          low: 0.01,
+          average: 0.3,
+          high: 0.04
+      }
+    });
+
+    await window.keplr.enable(chainId);
+
+    refreshKeplr();
+  }
+
   const data = [
     {
       img: img1,
@@ -102,12 +224,19 @@ const WalletConnect = () => {
       onClick: connectWalletHandler,
       address: web3Account,
     },
+    // {
+    //   img: img2,
+    //   title: "Terra Station",
+    //   description: "Terra Testnet",
+    //   onClick: connectTerra,
+    //   address: wallets[0]?.terraAddress,
+    // },
     {
       img: img2,
-      title: "Terra Station",
-      description: "Terra Testnet",
-      onClick: connectTerra,
-      address: wallets[0]?.terraAddress,
+      title: "Keplr",
+      description: "Terra Bombay Testnet",
+      onClick: connectKeplr,
+      address: keplrAddress,
     },
     // {
     //     img: img3,
@@ -143,6 +272,7 @@ const WalletConnect = () => {
 
   useEffect(() => {
     connectWalletHandler();
+    refreshKeplr();
   }, [])
 
   return (
