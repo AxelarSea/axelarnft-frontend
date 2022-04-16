cdb094a66e185a1f153c412252a0c3bc77cee01f
Siravitch Tantra <siravitch.pe@gmail.com>
2022-04-16 23:14:45 +0700

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -14,86 +14,31 @@ const menus = [
     //             sub: 'Home 02',
     //             links: '/home-02'
     //         },
-    //         {
-    //             id: 3,
-    //             sub: 'Home 03',
-    //             links: '/home-03'
-    //         },
-    //         {
-    //             id: 4,
-    //             sub: 'Home 04',
-    //             links: '/home-04'
-    //         },
-    //         {
-    //             id: 5,
-    //             sub: 'Home 05',
-    //             links: '/home-05'
-    //         },
-    //         {
-    //             id: 6,
-    //             sub: 'Home 06',
-    //             links: '/home-06'
-    //         },
-    //         {
-    //             id: 7,
-    //             sub: 'Home 07',
-    //             links: '/home-07'
-    //         },
-    //         {
-    //             id: 8,
-    //             sub: 'Home 08 (Special)',
-    //             links: '/home-08'
-    //         },
     //     ]
     // },
     {
-        id: 2,
+        id: 1,
         name: 'Explore',
         links: '/Explore',
         
     },{
-        id: 3,
+        id: 2,
         name: 'Create',
         links: '/create-item'
        
     },
     {
-        id: 4,
+        id: 3,
         name: 'Profile',
         links: '/Authors-Profile',
         
     },
     {
-        id: 5,
+        id: 4,
         name: 'Faucet',
         links: '/faucet',
         
     },
-    // {
-    //     id: 6,
-    //     name: 'Docs',
-    //     links: '',
-        
-    // },
-
-    // {
-    //     id: 7,
-    //     name: 'Contact',
-    //     links: '/contact-01',
-    //     namesub: [
-    //         {
-    //             id: 1,
-    //             sub: 'Contact 1',
-    //             links: '/contact-01'
-    //         },
-    //         {
-    //             id: 2,
-    //             sub: 'Contact 2',
-    //             links: '/contact-02'
-    //         }
-    //     ],
-    // },
-    
 ]
 
 export default menus;
