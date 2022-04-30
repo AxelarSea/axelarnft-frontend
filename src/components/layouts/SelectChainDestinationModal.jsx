7dd3b0d1644eda235e837ca9fbf42be6f79a9562
Siravitch Tantra <siravitch.pe@gmail.com>
2022-04-30 13:12:01 +0700

    Update SelectChainDestinationModal.jsx

--- src/components/layouts/SelectChainDestinationModal.jsx
+++ src/components/layouts/SelectChainDestinationModal.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import { Modal } from "react-bootstrap";
 
 
-import ethLogo from '../../assets/images/icon/eth-logo.png'
-import moonbeamLogo from '../../assets/images/icon/moonbeam-logo.png'
-import polygonLogo from '../../assets/images/icon/polygon-logo.png'
-import avaxLogo from '../../assets/images/icon/avax-logo.png'
-import fantomLogo from '../../assets/images/icon/fantom-logo.png'
+import ethLogo from '../../assets/images/icon/eth-logo.svg'
+import moonbeamLogo from '../../assets/images/icon/moonbeam-logo.svg'
+import polygonLogo from '../../assets/images/icon/polygon-logo.svg'
+import avaxLogo from '../../assets/images/icon/avax-logo.svg'
+import fantomLogo from '../../assets/images/icon/fantom-logo.svg'
 
 const SelectChainDestinationModal = (props) => {
 
