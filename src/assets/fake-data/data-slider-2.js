6430b4c52212cba93e60fe181cff24ebc3b5a1fa
Siravitch Tantra <siravitch.pe@gmail.com>
2022-05-01 20:33:10 +0700

    Update data-slider-2.js

--- src/assets/fake-data/data-slider-2.js
+++ src/assets/fake-data/data-slider-2.js
@@ -4,9 +4,9 @@ import img1 from '../images/slider/axelarseaball.png';
 const heroSliderData = [
     {
         title_1: "First Cross-Chain",
-        title_2: "NFT Marketplace",
-        title_3: "Discover & Sell NFTs From Any Chain",
-        description: "Trade NFTs Using Any Token. From Any Chain",
+        title_2: "NFT Bridge & Marketplace",
+        title_3: "Unleashing the hidden potential of your NFTs",
+        description: "Unshackle your NFTs from the constraints of chains. Trades with any tokens from any chains. Bridge NFTs with full utilization.",
         img: img1,
         class: 'left'
     },
