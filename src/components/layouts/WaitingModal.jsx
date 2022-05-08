19cbf6ddd07a08e03b2df114669aacae8331c3ff
Siravitch Tantra <siravitch.pe@gmail.com>
2022-05-09 05:23:14 +0700

    Create WaitingModal.jsx

---
+++ src/components/layouts/WaitingModal.jsx
@@ -0,0 +1,16 @@
+import Countdown from "react-countdown";
+import { Modal } from "react-bootstrap";
+
+const WaitingModal = (props) => {
+
+    return (
+    <Modal show={props.onShow} onHide={props.onHide}>
+      <Modal.Header closeButton></Modal.Header>
+      <div className="d-flex flex-column  pd-20">
+        <h1 className="center">Time remaining</h1>
+        <Countdown className="center countdown" date={Date.now() + 300000}></Countdown>
+      </div>  
+    </Modal>
+    )
+}
+export default WaitingModal;
