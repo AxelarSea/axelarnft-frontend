2adc3e2c1094bb425121d944c6723e2b910a1724
Siravitch Tantra <siravitch.pe@gmail.com>
2022-05-12 19:32:33 +0700

    Update TranferFailedModal.jsx

--- src/components/layouts/TranferFailedModal.jsx
+++ src/components/layouts/TranferFailedModal.jsx
@@ -7,23 +7,23 @@ const TransferFailedModal = (props) => {
       <Modal.Header closeButton></Modal.Header>
       <div className="center pd-20">
         <img src={closeImg} alt="" width="200px" />
-        <h2 style={{ color: "grey", marginTop: "3rem" }}>
+        <h3 style={{ color: "grey", marginTop: "3rem" }}>
           {props.status < 1
-            ? "Axelar Error"
+            ? "Axelar Network Unresponsive"
             : props.status < 2
             ? "Insufficient fund or gas!"
             : props.status < 4
             ? "Axelar Network Unresponsive"
             : "Transfer Failed"}
-        </h2>
-        <p style={{ color: "grey", marginTop: "3rem", marginBottom: "3rem" }}>
+        </h3>
+        <p style={{ color: "grey", marginTop: "2rem", marginBottom: "3rem", fontSize:'13px', lineHeight:'20px'}}>
           {props.status < 1
-            ? "Axelar Network Unresponsive Fail to get a deposit address. Please try again later in 30 minutes."
+            ? <a>Axelar Network Unresponsive Fail to get a deposit address. Please try again later in 30 minutes.</a>
             : props.status < 2
-            ? "Please request luna from faucet and swap some luna to ust! Or you have cancelled your transaction."
+            ? <a>Please request luna from faucet and swap some luna to ust! Or you have cancelled your transaction.</a>
             : props.status < 4
-            ? "Axelar Network Unresponsive It takes longer than expected for Axelar Network to transfer your funds. Your assets are safe on the destination chain and available for purchase in the next transaction. Please try again later in 30 minutes."
-            : "NFT is no longer available Oh no! Someone else just snatch the NFT. Your funds are safe on the destination chain and available for your next purchase."}
+            ? <a>Axelar Network Unresponsive It takes longer than expected for Axelar Network to transfer your funds. <a className="animation-gradient" style={{fontWeight:'600'}}>Your assets are safe on the destination chain</a> and available for purchase in the next transaction. Please try again later in 30 minutes.</a>
+            : <a>NFT is no longer available Oh no! Someone else just snatch the NFT. Your funds are safe on the destination chain and available for your next purchase.</a>}
         </p>
         <button onClick={props.onHide}>Refresh</button>
       </div>
