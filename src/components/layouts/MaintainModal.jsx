9efacf6900613dc12df82797bf03727147c7b8b9
Chomtana <Chomtana001@gmail.com>
2022-06-04 21:59:34 +0700

    Update modal text

--- src/components/layouts/MaintainModal.jsx
+++ src/components/layouts/MaintainModal.jsx
@@ -3,6 +3,22 @@ import { Modal } from "react-bootstrap";
 import maintainImg from "../../assets/images/icon/maintain.svg";
 
 const MaintainModal = (props) => {
+  if (parseInt(process.env.REACT_APP_UNDER_MAINTENANCE_GLOBAL)) {
+    <Modal show={props.onShow} onHide={props.onHide}>
+      <Modal.Header closeButton></Modal.Header>
+      <div className="center p-4">
+        <img src={maintainImg} alt="" width="200px" />
+        <h2 className="my-4">AxelarSea is under maintenance</h2>
+        <p>
+          We are optimizing our backend service.
+        </p>
+        <p>
+          Estimated completion: 7 June 2022
+        </p>
+      </div>
+    </Modal>
+  }
+
   return (
     <Modal show={props.onShow} onHide={props.onHide}>
       <Modal.Header closeButton></Modal.Header>
