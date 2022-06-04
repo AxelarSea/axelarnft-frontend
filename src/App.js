901baf92749d784153326a3015691c6c4080e950
Chomtana <Chomtana001@gmail.com>
2022-06-04 16:38:43 +0700

    Add global maintain modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import './style.css';
-import './App.css';
-import {   Routes , Route } from 'react-router-dom';
-import routes from './pages/index'
-
+import "./style.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import routes from "./pages/index";
 
 function App() {
+  return (
+    <>
+      <Routes>
+        {routes.map((data, index) => (
+          <Route
+            onUpdate={() => window.scrollTo(0, 0)}
+            exact={true}
+            path={data.path}
+            element={data.component}
+            key={index}
+          />
+        ))}
 
-    return (
-        <Routes >
-            {
-            routes.map((data,index) => (
-                <Route onUpdate={() => window.scrollTo(0, 0)} exact={true} path={data.path} element={data.component} key={index} />
-            ))
-            }
+        <MaintainModal
+          onShow={Boolean(parseInt(process.env.REACT_APP_UNDER_MAINTENANCE_GLOBAL))}
+          onHide={() => {}}
+        />
       </Routes>
-    );
+    </>
+  );
 }
 
 export default App;
